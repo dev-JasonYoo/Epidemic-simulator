a5a6ed6d2762fbddc8343c3cd8e2d55c3cef83a7
dev-JasonYoo <jasonshyoo@gmail.com>
2022-11-14 14:24:28 -0500

    first commit

---
+++ main_test.py
@@ -0,0 +1,20 @@
+from lib import *
+
+## Initialize Person instances
+population = [ Person(ID_num, 0, 0, 0) for ID_num in range(100) ]
+
+##corona = Epid(0)
+##population[0].set_status(corona)
+
+## Initialize Place instances
+square1 = Place('square1', 10, [10, 10], 0, [])
+square2 = Place('square2', 10, [10, 10], 0, ['square1'])
+
+##square1_dimension = square1.dimension
+##for person in population[:10]:
+##    x = randint(0, square1_dimension[0] - 1)
+##    y = randint(0, square1_dimension[1] - 1)
+##    square1.field[x][y] = person
+
+assign(population[:20], square1)
+print_field(square1)
