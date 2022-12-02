1e2812889c48b2aa5aeb65532e95c38491194d6a
dev-JasonYoo <jasonshyoo@gmail.com>
2022-12-01 19:34:20 -0500

    add file output

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 
 n=270
 n0 = 3
-corona = repr(Epid(0.1, incubation_days = 4))
+corona = repr(Epid(0.2, incubation_days = 4))
 
 def test(quarantine = True):
     population = [ Person(ID_num, 0, 0, 0) for ID_num in range(n) ]
@@ -49,44 +49,60 @@ def test(quarantine = True):
     }
     return result
 
-def animate(result):
-    fig = plt.figure(figsize=(15,5))
-    ax1 = fig.add_subplot(1,2,1)
-    ax1.axis(ymin = -5, ymax = 275)
-    ax2 = fig.add_subplot(1,2,2)
-    ax2.axis(ymin = -5, ymax = 275)
+
+def test_log(file_name: str, quarantine = True):
+    #file_name should end with '.txt'
+    population = [ Person(ID_num, 0, 0, 0) for ID_num in range(n) ]
+
+    # assign initial cases
+    for idx in range(n0):
+        population[idx].status = eval(corona)
+
+    # initialize places and assign people to them
+    square1 = Place('square1', 10, [30, 30], 0, [])
+    init_assign(population, square1, init = True)
     
-    x = []
-    S, I, R = [], [], []    
+    day = 0
+    susceptible = [n - n0] # S
+    infectious = [n0] # I
+    recovered = [0] # R
+    with open(file_name, 'w') as f:
+        f.write(f"n: {n}\nn0: {n0}\ncorona: {repr(corona)}\n")
+        f.write(f"Place: {Place.get_place_list()}\n")
+        if quarantine:
+            f.write("Quarantine is required\n")
+        else:
+            f.write("Quarantine is not required\n")
+        f.write("\n")
+        while True:
+            day += 1
+            square1.field = update_position_in_place(population[:n], square1)
+            result = spread_epid(population[:n], quarantine)
 
-    def update(result, frame):
-        x.append(frame)
-        S.append(result['S'][frame])
-        I.append(result['I'][frame])
-        R.append(result['R'][frame])
-        
-        ax1.clear()
-        ax2.clear()
-        
-        ax1.bar(x, S, color = 'gray')
-        ax1.bar(x, I, bottom = np.array(S), color = 'red')
-        ax1.bar(x, R, bottom = np.array(S) + np.array(I), color = 'blue')
+            # result: new_case_num, infectious_num, quarantine_num, new_recovered_num        
+            susceptible.append(susceptible[-1] - result['new_case_num'])
+            infectious.append(infectious[-1] + result['new_case_num'] - result['new_recovered_num'])
+            recovered.append(recovered[-1] + result['new_recovered_num'])
+            f.write(f'Day {day}:\tS{susceptible[-1]}\tI{infectious[-1]}\tR{recovered[-1]}\n')
 
-        ax2.plot(S, color = 'gray')
-        ax2.plot(I, color = 'red')
-        ax2.plot(R, color = 'blue')
-        
-    def animate_helper(result):
-        ani = animation.FuncAnimation(
-            fig,
-            partial(update, result),
-            frames = list(range(len(result['S']))),
-            interval = 100,
-            repeat = False
-        )
-        plt.show()
+            # determine if epidemic went extinct
+            if result['new_case_num'] == 0:
+                if result['infectious_num'] == 0:
+                    if result['quarantine_num'] == 0:
+                        break
 
-    animate_helper(result)
+        result = {
+            'S': susceptible,
+            'I': infectious,
+            'R': recovered,
+        }
+        f.write(f"\nSusceptible:\n{result['S']}\n")
+        f.write(f"Infectious:\n{result['I']}\n")
+        f.write(f"Recovered:\n{result['R']}\n\n")
+
+        f.write(f"{int(result['R'][-1] / n * 100)} percentage of population got infected.\n")
+        f.write(f"The disease vanished in {day} days.")
+    return result
 
     
 def test_visible(quarantine = True):
@@ -114,12 +130,11 @@ def test_visible(quarantine = True):
     day = 0
     while True:
         day += 1
-        print('Day ', day)
         square1.field = update_position_in_place(population[:n], square1)
 
         # ---- turtle ----
         t.write(str_field_infect(square1) + f"Day {day+1}", font = ('Arial', 7, 'normal'))
-        sleep(0.05)
+        sleep(0.4)
         # ---- turtle ----
         
         result = spread_epid(population[:n], quarantine)
@@ -132,14 +147,56 @@ def test_visible(quarantine = True):
 
     return [person.status for person in population].count(True), f"Day {day}"
 
-def histogram(result_list: list):
-    keys = [x for x in range(0,271,10)]
-    result_list = list(map(lambda x: x//10 * 10, result_list))
-    histogram = dict.fromkeys(keys, 0)
-    for result in result_list:
-        histogram[result] += 1    
-    return histogram
+
+# -------- Live update plot --------
 
 
-result = test(False)
+def animate(result):
+    fig = plt.figure(figsize=(15,5))
+    fig.suptitle('Epidemic dynamics', fontsize = 20)
+    
+    ax1 = fig.add_subplot(1,2,1)
+    ax1.axis(ymin = -5, ymax = n+5)
+    ax1.set_xlabel('Days since onset', fontsize = 12)
+    ax1.set_ylabel('Population')
+    
+    ax2 = fig.add_subplot(1,2,2)
+    ax2.axis(ymin = -5, ymax = n+5)
+    ax2.set_xlabel('Days since onset')
+    ax2.set_ylabel('Population')
+    
+    x = []
+    S, I, R = [], [], []    
+
+    def update(result, frame):
+        x.append(frame)
+        S.append(result['S'][frame])
+        I.append(result['I'][frame])
+        R.append(result['R'][frame])
+        
+        ax1.clear()
+        ax2.clear()
+        
+        ax1.bar(x, S, color = 'gray')
+        ax1.bar(x, I, bottom = np.array(S), color = 'red')
+        ax1.bar(x, R, bottom = np.array(S) + np.array(I), color = 'blue')
+
+        ax2.plot(S, color = 'gray', label = 'Susceptible')
+        ax2.plot(I, color = 'red', label = 'Infectious')
+        ax2.plot(R, color = 'blue', label = 'Recovered')
+        ax2.legend(loc = 'upper right')
+        
+    def animate_helper(result):
+        ani = animation.FuncAnimation(
+            fig,
+            partial(update, result),
+            frames = list(range(len(result['S']))),
+            interval = 100,
+            repeat = False
+        )
+        plt.show()
+
+    animate_helper(result)
+
+result = test(True)
 animate(result)
