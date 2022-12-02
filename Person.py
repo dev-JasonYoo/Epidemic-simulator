c4d00bd29700b9e3a42d0d60720cbcb959961756
dev-JasonYoo <jasonshyoo@gmail.com>
2022-12-02 01:46:12 -0500

    Refactoring and added docstring for submission

--- Person.py
+++ Person.py
@@ -1,45 +1,74 @@
 from Epid import *
 
 class Person:
-    def __init__(self, ID_num, name, age, place):
+    ''' Information about Person class
+
+    Attributes:
+
+    __ID_num
+        int
+
+    __place
+        bool or Place
+
+    __status
+        bool or Epid
+    '''
+    def __init__(self, ID_num: int, name: str, age: int, place):
+        ''' Initialize self,
+
+        Person, int, str, int, Place -> None '''
         self.__ID_num = ID_num
-        self.__name = name
-        self.__age = age
+        self.__name = name # not used (used only in initialization)
+        self.__age = age # not used (used only in initialization)
         self.__place= place
         self.__status = Epid.susceptible
 
     def __repr__(self):
-        try:
-            return f"Person({self.__ID_num}, {self.__name}, {self.__age}, {self.__place}, {self.__status})"
-        except TypeError:
-            print(f"Person({self.__ID_num}, {self.__name}, {self.__age}, {self.__place}")
+        ''' Returns the representation of this Person object
+
+        Person -> str '''
+        return f"Person({self.__ID_num}, {self.__name}, {self.__age}, {self.__place}, {self.__status})"
+
+    def __str__(self):
+        ''' Returns the string form of this Person object
+
+        Person -> str '''
+        return f"Person({self.__ID_num}, {self.__name}, {self.__age}, {self.__place}, {self.__status})"
 
     @property
     def ID_num(self):
+        ''' Getter method of attribute ID_num by property decorator
+
+        Person -> int '''
         return self.__ID_num
-
-    def get_name(self):
-        return self.__name
-
-    def get_age(self):
-        return self.__age
-
-    def add_age(self):
-        self.__age += 1
-        return None
 
     @property
     def place(self):
+        ''' Getter method of attribute place by property decorator
+
+        Person -> bool or Place '''
         return self.__place
 
     @place.setter
     def place(self, value):
+        ''' Setter method of attribute place by property decorator
+
+        Person, bool or Place -> None '''
         self.__place = value
+        return None
 
     @property
     def status(self):
+        ''' Getter method of attribute status by property decorator
+
+        Person -> bool or Epid '''
         return self.__status
 
     @status.setter
-    def status(self, status):
-        self.__status = status
+    def status(self, value):
+        ''' Setter method of attribute status by property decorator
+
+        Person, bool or Epid -> None '''
+        self.__status = value
+        return None
